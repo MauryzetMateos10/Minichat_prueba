25ed1097d0c77d663eefb7137c36d738f75bd84f
MauryzetMateos10 <152357902+MauryzetMateos10@users.noreply.github.com>
2024-10-09 15:51:37 -0600

    chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:5000');
+var socket = io.connect('http://3.81.10.145:5000');
 
 var persona = document.getElementById('persona'),
     contrasena = document.getElementById('contraseña'),
